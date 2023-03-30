3857b31ff9f2f3be20535b53b8f6f5760ac46b38
ANKITA SAHOO <ankitacoder3@gmail.com>
2023-03-30 14:33:24 +0530

    Add files via upload

---
+++ Python_Basics/Iterative_Constructs_Lists_Tuples_1/Program3.py
@@ -0,0 +1,54 @@
+'''
+Problem Statement:
+Generate heart rate randomly between 50 to 120 at time interval of 3 hours for 24 hours.
+(i) If heart rate is between 50-65 print as bradycardia(lower heart rate) if greater than 100 print as tachycardia(higher heart rate). Else print as normal.
+(ii) Count number of Bradycardia and tachycardia if any of this is greater than 3 display as risk.
+(iii) Print the maximum heart rate and minimum heart rate
+'''
+
+#getting the random heartbeat
+import random
+heart_bt = [ random.randrange (50,120) for i in range(1, 24, 3) ]
+print(f"The heartbeats are as follows: {heart_bt} ")
+
+#for bradycardia and tachycardia the count variables are, respectively.
+b_count = 0
+t_count = 0
+
+#define max and min
+max = 200
+min = 0
+
+#using the for loop, and if conditions.
+for x in heart_bt:
+
+    #bradycardia
+    if x in range(50, 66):
+        print(f" Bradycardia occurs at {x}")
+        b_count +=1
+        
+    #tachycardia
+    elif x >100:
+        print(f" Tachycardia occurs at {x}")
+        t_count +=1
+
+    #normal
+    else :
+        print(f" The heartbeat is normal at {x}")
+        t_count +=1
+
+    # getting min and max values
+    if x< max:
+        max = x
+    if x> min:
+        min= x
+
+#final result or state
+if b_count>3 or t_count>3 :
+    print("Heart is at RISK!")
+
+print(f" The  maximum heart rate is {max}, and the minimum heart rate is {min}.")
+    
+
+
+
---
+++ Python_Basics/Iterative_Constructs_Lists_Tuples_1/Program4.py
@@ -0,0 +1,56 @@
+'''
+Problem Statement:
+Enter marks of students till you need to stop. 
+a) Find maximum marks
+b) Find number of students who have scored highest
+c) Find second highest marks
+d) Enter fail marks and remove if fail marks present in list
+'''
+
+#getting input for no. of students, n
+n =int(input("Enter number of students: "))
+
+#entering the marks for n students
+x=0
+marks=[]
+while(x<n):
+        Y= int(input(f"Enter marks for student {x}:"))
+        marks.append(Y)
+        x=x+1
+
+#sorting list
+marks= sorted(marks)
+print(sorted(marks))
+
+#the maximum marks
+mx = marks[n-1]
+print("The maximum or highest mark scored by a student is: ",mx)
+
+#no. of students that scored maximum marks
+mx_count=0
+for mark in marks:
+        if mark==mx:
+                mx_count+=1    
+print("The number of students that scored maximum marks is/are:",mx_count)
+
+#the second highest marks
+mx2 = marks[n-2]
+print("The second highest mark scored by a student is:",mx2)
+
+#removing the marks which are below pass marks or have failed
+#by defining a new empty list, final_list
+final_list = []
+fail_mark = 40
+for mark in marks:
+        if mark > fail_mark:
+                final_list.append(mark)
+        else:
+                print("The marks that is below or equal to fail is/are:", mark)
+        
+#printing the final list
+print("The final list is:",final_list)
+
+
+
+
+
