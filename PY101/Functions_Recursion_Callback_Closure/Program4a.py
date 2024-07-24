69d657be98d00d3cebe463f589849170b290e10b
ANKITA SAHOO <ankitacoder3@gmail.com>
2024-07-25 02:47:18 +0530

    Add files via upload

--- PY101/Functions_Recursion_Callback_Closure/Program4a.py
+++ PY101/Functions_Recursion_Callback_Closure/Program4a.py
@@ -3,7 +3,7 @@ Problem Statement:
 Increment a given number by 5, for n number of times,
 using Closure.
 '''
-print('\nCalculate Callbacks.py\n')
+print('\nCalculate Closure_increment_N_times.py\n')
 
 #Defining the Count function. 
 
@@ -15,14 +15,15 @@ def Count(n):
 
 #Getting the input, a number.
 print('Enter a number \'n\', to be incremented by 5, \'n\' no. of times.\n')
-n=int(input('Enter the number, n:'))
+n=int(input('Enter the number, n: '))
+print()
 val=n
 
 #Printing the output, by using 'for' loop and, by calling the function (by using 'closure concept').
 for i in range(n):
     c=Count(val) #c refers to Count
     val=c() #to get value in c, need to call c [c()]
-    print(f'Increment-{i}, value: {val}')
+    print(f'After Increment No. -> {i+1}, value of number n: {val}')
 
-print('\nFinal Result:',val)
+print('\nFinal Result:\n After',n,'increments to the number',n,',\n \t \t the result is -> ',val)
 print()
